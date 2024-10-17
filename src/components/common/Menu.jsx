90d3df35b4257fb519f6e8be0b44d5125cebeb30
Matias Salazar <matias39974593@gmail.com>
2024-10-17 00:11:04 -0300

    se realizo el navbar y se dio un poco de estilos

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Menu = () => {
-    return (
-        <div>
-            <h2>Navbar</h2>
-        </div>
-    );
+  return (
+    <Navbar expand="lg" className="custom-navbar bg-light">
+      <Container>
+        <Row className="w-100 align-items-center">
+          <Col xs={4} className="text-end">
+            <Nav className="justify-content-end">
+              <Nav.Link href="#" className="nav-item">
+                INICIO
+              </Nav.Link>
+              <Nav.Link href="#" className="nav-item">
+                RESERVAS
+              </Nav.Link>
+              <Nav.Link href="#" className="nav-item">
+                NOSOTROS
+              </Nav.Link>
+              <Nav.Link href="#" className="nav-item">
+                CONTACTOS
+              </Nav.Link>
+            </Nav>
+          </Col>
+          <Col xs={4} className="text-center">
+            <Navbar.Brand href="#" className="hotel-title">
+              HOTEL
+            </Navbar.Brand>
+          </Col>
+          <Col xs={4} className="text-start">
+            <Nav className="justify-content-start">
+              <Nav.Link href="#" className="nav-item active">
+                REGISTRARSE
+              </Nav.Link>
+              <Nav.Link href="#" className="nav-item active">
+                ADMINISTRACION
+              </Nav.Link>
+              <Nav.Link href="#" className="nav-item active">
+                INICIAR SESION
+              </Nav.Link>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
