9b75e8755c72b840c6277fecf2e907dbd7005b5d
Matias Salazar <matias39974593@gmail.com>
2024-10-17 00:48:23 -0300

    se realizo el footer y se sio estilos personalizados

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,46 +1,29 @@
-import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
+import { Navbar, Nav, Container} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Menu = () => {
   return (
-    <Navbar expand="lg" className="custom-navbar bg-light">
+    <Navbar bg="light" expand="lg" className="custom-navbar">
       <Container>
-        <Row className="w-100 align-items-center">
-          <Col xs={4} className="text-end">
-            <Nav className="justify-content-end">
-              <Nav.Link href="#" className="nav-item">
-                INICIO
-              </Nav.Link>
-              <Nav.Link href="#" className="nav-item">
-                RESERVAS
-              </Nav.Link>
-              <Nav.Link href="#" className="nav-item">
-                NOSOTROS
-              </Nav.Link>
-              <Nav.Link href="#" className="nav-item">
-                CONTACTOS
-              </Nav.Link>
-            </Nav>
-          </Col>
-          <Col xs={4} className="text-center">
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#services">Reservas</Nav.Link>
+            <Nav.Link href="#about">Nosotros</Nav.Link>
+          </Nav>
+          <Nav className="mx-auto">
             <Navbar.Brand href="#" className="hotel-title">
               HOTEL
             </Navbar.Brand>
-          </Col>
-          <Col xs={4} className="text-start">
-            <Nav className="justify-content-start">
-              <Nav.Link href="#" className="nav-item active">
-                REGISTRARSE
-              </Nav.Link>
-              <Nav.Link href="#" className="nav-item active">
-                ADMINISTRACION
-              </Nav.Link>
-              <Nav.Link href="#" className="nav-item active">
-                INICIAR SESION
-              </Nav.Link>
-            </Nav>
-          </Col>
-        </Row>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link href="#gallery">Contactos</Nav.Link>
+            <Nav.Link href="#contact">Administracion</Nav.Link>
+            <Nav.Link href="#book">Registrarse</Nav.Link>
+            <Nav.Link href="#book">Iniciar sesion</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
