3fe79fe4b193a76048786ca8a8799564215337a9
SantinoHamada <hamadasantino@hotmail.com>
2024-10-17 01:03:41 -0300

    Se realizó la logica y validaciones para iniciarSesion. falta añadir mas estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
 import Footer from './components/common/Footer'
 import Menu from './components/common/Menu'
+import IniciarSesion from './components/pages/IniciarSesion'
 import Inicio from './components/pages/Inicio'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -12,6 +15,7 @@ function App() {
     <Menu></Menu>
     <Routes>
       <Route exact path="/" element={<Inicio></Inicio>}></Route>
+      <Route exact path="/iniciarSesion" element={<IniciarSesion></IniciarSesion>}></Route>
     </Routes>
     <Footer></Footer>
     </BrowserRouter>
