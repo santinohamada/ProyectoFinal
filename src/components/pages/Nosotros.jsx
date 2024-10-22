198cadcbdc1bad6b20c5aaa29cce7f6d02364760
Felicitas Ralle <felicitasralle@gmail.com>
2024-10-21 21:24:10 -0300

    se agrego un carrousel de imagenes del personal del hotel

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,26 +1,58 @@
 import React from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import nosotros1 from "../../assets/nosotros1.jpg";
 import nosotros2 from "../../assets/nosotros2.jpg";
+import nosotros3 from "../../assets/nosotros3.jpg";
 
 const Nosotros = () => {
   return (
     <section className="container-fluid row justify-content-center text-center fondoNosotros">
-      <article className=" col-12 col-md-6 col-lg-4 p-3">
-        <img
-          src={nosotros1}
-          alt="Recepcion del hotel"
-          className="img-fluid rounded-circle mb-4 shadow"
-        />
-      </article>
-      <article className=" col-12 col-md-6 col-lg-4 p-3">
-        <img
-          src={nosotros2}
-          alt="Huesped del hotel"
-          className="img-fluid rounded-circle mb-4 shadow"
-        />
-      </article>
       <article className="col-12 col-md-6 col-lg-4 p-3">
+        <div
+          id="carouselExampleRide"
+          className="carousel slide"
+          data-bs-ride="true"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={nosotros1} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src={nosotros2} className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src={nosotros3} className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleRide"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleRide"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </article>
+      <article className="col-12 col-md-6 col-lg-4 p-3 mt-5 ms-5">
         <h2 className="mb-4 tituloNosotros">Quienes Somos</h2>
         <p className="textoNosotros">
           En (Nombre del Hotel), creemos que el verdadero lujo reside en la
