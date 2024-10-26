dca26d7c75b8e2c8ef395b16b72cdb10830ecaf6
Matias Salazar <matias39974593@gmail.com>
2024-10-26 16:03:38 -0300

    se realizó cambios en el footer con nuevos estilos y diseño

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,40 +1,49 @@
 import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Footer = () => {
   return (
-    <footer className="custom-footer bg-light text-dark py-4">
+    <footer className="custom-footer bg-dark text-light py-4">
       <Container>
-        <Row>
-          <Col md={4} className="text-center mb-3">
-            <h5 className="hotel-title-footer">HOTEL</h5>
-            <p>Experiencia única de confort y relajación.</p>
-          </Col>
-          <Col md={4} className="text-center mb-3">
-            <h6>Contacto</h6>
-            <p>Tel: ## </p>
-            <p>Dirección: Tucumán, Argentina</p>
-          </Col>
-          <Col md={4} className="text-center mb-3">
-            <h6>Síguenos</h6>
-            <div className="social-links d-flex justify-content-center">
-              <a href="#" className="social-icon">
-                Facebook
-              </a>
-              <a href="#" className="social-icon">
-                Instagram
-              </a>
-              <a href="#" className="social-icon">
-                Twitter
-              </a>
-            </div>
+        <Row className="justify-content-center">
+          <Col
+            md={8}
+            className="d-flex justify-content-center align-items-center"
+          >
+            <a href="#" className="social-icon mx-2">
+              <i className="bi bi-facebook"></i>
+            </a>
+            <a href="#" className="social-icon mx-2">
+              <i className="bi bi-instagram"></i>
+            </a>
+            <a href="#" className="social-icon mx-2">
+              <i className="bi bi-youtube"></i>
+            </a>
+            <a href="#" className="social-icon mx-2">
+              <i className="bi bi-twitter"></i>
+            </a>
+            <a href="#" className="social-icon mx-2">
+              <i className="bi bi-linkedin"></i>
+            </a>
           </Col>
         </Row>
-        <Row>
-          <Col className="text-center mt-3">
+        <Row className="justify-content-center mt-3">
+          <Col className="text-center">
+            <h5 className="footer-brand my-4">HOTEL PATAGONIA</h5>
             <p className="mb-0">
-              &copy; 2024 HOTEL. Todos los derechos reservados.
+              &copy; 2024 HOTEL PATAGONIA. Todos los derechos reservados.
             </p>
+          </Col>
+        </Row>
+        <Row className="justify-content-center mt-3">
+          <Col className="text-center">
+            <a href="#" className="footer-link mx-3">
+              Información legal
+            </a>
+            <a href="#" className="footer-link mx-3">
+              Política de privacidad
+            </a>
           </Col>
         </Row>
       </Container>
