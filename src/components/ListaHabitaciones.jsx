cef91700fea676045b6c66072c6e244370ec1454
Gustavo Gettar <gus.gettar@gmail.com>
2024-10-28 09:37:53 -0300

    Lista habitaciones

--- src/components/ListaHabitaciones.jsx
+++ src/components/ListaHabitaciones.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { obtenerReservas, reservarHabitacion } from '../helpers/queries.js';
 
-const listaHabitaciones = () => {
+
+const ListaHabitaciones = ({habitacion,reserva}) => {
+  
+  
+    const estaReservada = reserva.includes(habitacion.roomNumber)
+    
+    
+
+
     return (
-        <div>
-            
+      <div className=' d-flex flex-column'>
+
+        <Card className=' card-administracion' style={{ width: '10rem'}}>
+      <Card.Img variant="top" className='card-img-fixed' src={habitacion.image} />
+      <Card.Body className='d-flex flex-column h-100'>
+
+        <Card.Title>{habitacion.roomNumber}</Card.Title>
+        
+        <Card.Text>
+          {habitacion.description}
+        </Card.Text>
+        
+        
+
+        <Button className='mt-auto' variant="primary">Ver</Button>
+        <div className={`badge ${estaReservada ? 'disponible' : 'no-disponible'}`}>
+          {estaReservada===null ? 'Cargando..': estaReservada ? 'Disponible' : 'No Disponible'}
+
         </div>
+       
+      </Card.Body>
+    </Card>
+      </div>
     );
 };
 
-export default listaHabitaciones;
+export default ListaHabitaciones;
